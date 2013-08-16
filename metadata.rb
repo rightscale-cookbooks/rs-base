913aa82c748ecab4956f16516ea5de2e1fa30503
Lopaka Delp <lopaka@rightscale.com>
2013-08-15 22:10:33 -0700

    acu112285 - created setup_ntp recipe wrapper and updated files accordingly

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,11 @@ maintainer_email 'cookbooks@rightscale.com'
 license          'Apache 2.0'
 description      'Installs/Configures rs-base'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue '0.1.0'
+version          '0.0.0'
 
+supports "centos"
+supports "ubuntu"
+
+depends "ntp"
+
+recipe "rs-base::setup_ntp", "Installs and configures ntp client."
