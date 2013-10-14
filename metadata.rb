8744614054320a7c82d31e65f459c52bd2942447
Douglas Thrift <douglas.thrift@rightscale.com>
2013-10-14 14:57:08 -0700

    Add ephemeral_lvm recipe and metadata.

--- metadata.rb
+++ metadata.rb
@@ -13,6 +13,7 @@ depends "ntp", "~> 1.4.0"
 depends "marker", "~> 0.1.0"
 depends "swap", "~> 0.3.5"
 depends "rsyslog", "~> 1.8.0"
+depends "ephemeral_lvm", "~> 0.1.0"
 
 recipe "rs-base::ntp", "Installs and configures ntp client."
 
@@ -34,4 +35,46 @@ attribute "rs-base/rsyslog_server",
   :description =>
     "The FQDN or IP address of the remote rsyslog server.  If blank no remote syslog server is setup.",
   :recipes => ["rs-base::syslog"],
+  :required => "optional"
+
+attribute "ephemeral_lvm/filesystem",
+  :display_name => "Ephemeral LVM Filesystem",
+  :description => "The filesystem to be used on the ephemeral volume",
+  :default => "ext4",
+  :recipes => ["rs-base::default"],
+  :required => "recommended"
+
+attribute "ephemeral_lvm/mount_point",
+  :display_name => "Ephemeral LVM Mount Point",
+  :description => "The mount point for the ephemeral volume",
+  :default => "/mnt/ephemeral",
+  :recipes => ["rs-base::default"],
+  :required => "recommended"
+
+attribute "ephemeral_lvm/volume_group_name",
+  :display_name => "Ephemeral LVM Volume Group Name",
+  :description => "The volume group name for the ephemeral LVM",
+  :default => "vg-data",
+  :recipes => ["rs-base::default"],
+  :required => "optional"
+
+attribute "ephemeral_lvm/logical_volume_size",
+  :display_name => "Ephemeral LVM Logical Volume Size",
+  :description => "The size to be used for the ephemeral LVM",
+  :default => "100%VG",
+  :recipes => ["rs-base::default"],
+  :required => "optional"
+
+attribute "ephemeral_lvm/logical_volume_name",
+  :display_name => "Ephemeral LVM Logical Volume Name",
+  :description => "The name of the logical volume for ephemeral LVM",
+  :default => "ephemeral0",
+  :recipes => ["rs-base::default"],
+  :required => "optional"
+
+attribute "ephemeral_lvm/stripe_size",
+  :display_name => "Ephemeral LVM Stripe Size",
+  :description => "The stripe size to be used for the ephemeral logical volume",
+  :default => "512",
+  :recipes => ["rs-base::default"],
   :required => "optional"
--- recipes/default.rb
+++ recipes/default.rb
@@ -17,6 +17,7 @@
 # limitations under the License.
 #
 
+include_recipe "ephemeral_lvm"
 include_recipe "rs-base::swap"
 include_recipe "rs-base::ntp"
 include_recipe "rs-base::syslog"
