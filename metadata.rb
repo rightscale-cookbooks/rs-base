bea6ec01bc6f0d98b93918a20827d2ad0c555314
Curt Wilhelm <curt@rightscale.com>
2016-10-12 16:58:24 -0600

    pin rsyslog for chef 11 compatiblity

--- metadata.rb
+++ metadata.rb
@@ -15,7 +15,7 @@ depends 'marker', '~> 1.0.1'
 depends 'rightscale_tag', '~> 1.2.1'
 depends 'machine_tag', '~> 1.2.1'
 depends 'swap', '~> 0.3.5'
-depends 'rsyslog', '~> 4.0.0'
+depends 'rsyslog', '~> 2.2.0'
 depends 'collectd', '~> 1.1.0'
 
 recipe 'rs-base::default', 'All-in-one recipe to run all recipes in rs-base cookbook.'
