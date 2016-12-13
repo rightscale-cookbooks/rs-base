a0710b4270223c07b423dd6ac4dd47d354233acd
Richard Shade <rshade@rightscale.com>
2016-12-13 14:17:30 -0600

    adding respond_to's and guards for chef12, updating kitchen for error

--- metadata.rb
+++ metadata.rb
@@ -5,8 +5,9 @@ license          'Apache 2.0'
 description      'Installs/Configures rs-base'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '2.0.0'
-issues_url       'https://github.com/rightscale-cookbooks/rs-base/issues'
-source_url       'https://github.com/rightscale-cookbooks/rs-base'
+issues_url       'https://github.com/rightscale-cookbooks/rs-base/issues' if respond_to?(:issues_url)
+source_url       'https://github.com/rightscale-cookbooks/rs-base' if respond_to?(:source_url)
+chef_version     '>= 12.0' if respond_to?(:chef_version)
 
 supports 'centos'
 supports 'ubuntu'
