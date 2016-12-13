e6d8938fa1f319eece21c502fc776901f4c52ef9
Richard Shade <rshade@rightscale.com>
2016-12-13 12:47:17 -0600

    adding secrets recipe

--- test/cookbooks/fake/metadata.rb
+++ test/cookbooks/fake/metadata.rb
@@ -7,3 +7,4 @@ version          '0.1.0'
 
 recipe 'fake::rightscale_software', 'Installs the rightscale_software and rightscale_software_ubuntu repositories'
 recipe 'fake::tcpdump', 'Installs tcpdump'
+recipe 'fake::create_secrets', 'creates secrets file'
