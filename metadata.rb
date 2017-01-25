716ca5e3043e37065c457d5a43063a87f0ba9b94
Richard Shade <rshade@rightscale.com>
2017-01-25 09:00:51 -0600

    adding in sysctl files closes #36

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@rightscale.com'
 license          'Apache 2.0'
 description      'Installs/Configures rs-base'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.0.0'
+version          '2.1.0'
 issues_url       'https://github.com/rightscale-cookbooks/rs-base/issues' if respond_to?(:issues_url)
 source_url       'https://github.com/rightscale-cookbooks/rs-base' if respond_to?(:source_url)
 chef_version     '>= 12.0' if respond_to?(:chef_version)
@@ -20,9 +20,10 @@ depends 'rightscale_tag', '~> 2.0'
 depends 'machine_tag', '~> 2.0'
 depends 'swap'
 depends 'rsyslog'
-depends 'collectd', '~> 2.2.2'
-depends 'collectd_plugins', '~> 2.1.3'
+depends 'collectd', '~> 2.2'
+depends 'collectd_plugins', '~> 2.1'
 depends 'ephemeral_lvm', '2.0'
+depends 'sysctl'
 
 recipe 'rs-base::default', 'All-in-one recipe to run all recipes in rs-base cookbook.'
 recipe 'rs-base::ntp', 'Installs and configures ntp client.'
@@ -30,6 +31,7 @@ recipe 'rs-base::swap', 'Create and setup a swap file.'
 recipe 'rs-base::rsyslog', 'Install and setup rsyslog.'
 recipe 'rs-base::monitoring_collectd', 'Install and setup collectd and basic set of plugins'
 recipe 'rs-base::monitoring_rightlink', 'Configures rightlink default monitoring'
+recipe 'rs-base::sysctl', 'installs kernel tuning'
 
 attribute 'rs-base/ntp/servers',
   display_name: 'NTP Servers',
